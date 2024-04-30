042ec494dff33993499455fae34d7d2713aa95a5
Seliaste <aena.aria@protonmail.com>
2024-04-30 16:27:51 +0200

    Added the description of the alternative Frank way

--- src/classification.rs
+++ src/classification.rs
@@ -1,12 +1,17 @@
 /*
 This is an experimental classification technique.
-in an n-dimension cloud, we want to separate red points and black points. Red points will represent a class.
+In an n-dimension cloud, we want to separate red points and black points. Red points will represent a class.
 Black points need to be linked to a single red point. We don't want red points to be linked between them.
 Here, central face colors are red points and side colors are black points. Each red point has to be linked to k black points.
 To do this, we assign to every red point a list of the black points ordered by distance.
 If a black point is present in multiple lists of the k first points,
 then we keep only the one with the shortest distance and pop the other instances. We repeat this until there are no pops
 The class of the black point is determined by which list it appears in.
+
+Other way suggested by Frank:
+Instead of storing distances of every black points for each red point, we store distances of every red points for each black point.
+We start assigning black points that have the lowest distance to a red point, and once a red point has 8 elements we remove it from the assignable to.
+We are done once all the black points are assigned. We are then sure every point got assigned to a red point that has 8 elements or fewer.
  */
 
 use std::collections::HashMap;
@@ -39,13 +44,13 @@ pub struct Classification {
     red_points:Vec<Point>,
     black_points:Vec<Point>,
     distances:HashMap<Point,Vec<(f64,Point)>>,
-    per_red_points:i32
+    k:i32
 }
 
 impl Classification {
-    fn init(red_points:Vec<Point>, black_points:Vec<Point>, per_red_points:i32) -> Self {
+    fn init(red_points:Vec<Point>, black_points:Vec<Point>, k:i32) -> Self {
         let distances = HashMap::new();
-        Classification{red_points,black_points,distances,per_red_points}
+        Classification{red_points,black_points,distances,k}
     }
 
     fn calc_distances(&mut self) {
@@ -57,4 +62,9 @@ impl Classification {
             }
         }
     }
+
+    fn clear_doubles() {
+    }
+
+
 }
