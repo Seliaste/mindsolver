b465afbf16f09b3b6c0e0c6fc34c5a8e876df5fc
Seliaste <aena.aria@protonmail.com>
2024-05-27 14:33:07 +0200

    forgot to reset a change in the without hardware script

--- without_hardware.sh
+++ without_hardware.sh
@@ -1,2 +1,2 @@
 cargo build
-cargo run --target x86_64-unknown-linux-gnu -- --file scan_test_files/hard_to_solve.txt --nosolve
+cargo run --target x86_64-unknown-linux-gnu -- --file scan_test_files/solvable.txt --nosolve
