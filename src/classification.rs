508e148863afb3f93496e154eae703c47e356561
Seliaste <aena.aria@protonmail.com>
2024-07-02 11:16:16 +0200

    removed reduce R difference as it is no longer needed after normalization

--- src/classification.rs
+++ src/classification.rs
@@ -25,7 +25,7 @@ impl PartialEq for ColorPoint {
 
 impl ColorPoint {
     pub fn distance_to(&self, other: &Self) -> f64 {
-        ((self.r - other.r).powi(2) / 2. + (self.g - other.g).powi(2) + (self.b - other.b).powi(2))
+        ((self.r - other.r).powi(2) + (self.g - other.g).powi(2) + (self.b - other.b).powi(2))
             .sqrt()
     }
 
